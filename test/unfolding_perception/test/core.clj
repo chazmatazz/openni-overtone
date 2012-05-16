6c9fccdd2d411899fd89a368c31f490920e796c3
Charles Dietrich <charles.m.dietrich@gmail.com>
2012-05-16 08:13:37 -0600

    updated README & fixed deletions

--- test/unfolding_perception/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns unfolding-perception-clj.test.core
-  (:use [unfolding-perception-clj.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
